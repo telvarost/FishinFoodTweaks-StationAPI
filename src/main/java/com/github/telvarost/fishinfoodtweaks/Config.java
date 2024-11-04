d8d7c991c2a2b9d1287e5a4cfbd164bbaae268b6
telvarost <telontelvarost@gmail.com>
2024-11-03 20:21:14 -0500

    Migrate to GCAPI3

--- src/main/java/com/github/telvarost/fishinfoodtweaks/Config.java
+++ src/main/java/com/github/telvarost/fishinfoodtweaks/Config.java
@@ -1,39 +1,44 @@
 package com.github.telvarost.fishinfoodtweaks;
 
-import blue.endless.jankson.Comment;
-import net.glasslauncher.mods.api.gcapi.api.*;
+import net.glasslauncher.mods.gcapi3.api.*;
 
 public class Config {
 
-    @GConfig(value = "config", visibleName = "FishinFoodTweaks")
+    @ConfigRoot(value = "config", visibleName = "FishinFoodTweaks")
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
 
-        @ConfigName("Allow Catching Random Fish Sizes")
-        @MultiplayerSynced
+        @ConfigEntry(
+                name = "Allow Catching Random Fish Sizes",
+                multiplayerSynced = true
+        )
         public Boolean enableRandomFishSizes = true;
 
-        @ConfigName("Calculate Water Surface Size")
-        @Comment("Adjusts fish size/type based on area")
-        @MultiplayerSynced
+        @ConfigEntry(
+                name = "Calculate Water Surface Size",
+                description = "Adjusts fish size/type based on area",
+                multiplayerSynced = true
+        )
         public Boolean calculateWaterSurfaceSize = true;
 
-        @ConfigName("Display Fish Healing Tooltip")
+        @ConfigEntry(name = "Display Fish Healing Tooltip")
         public Boolean enableFishHealingTooltip = false;
 
-        @ConfigName("Display Food Healing Tooltips")
+        @ConfigEntry(name = "Display Food Healing Tooltips")
         public Boolean enableFoodHealingTooltips = false;
 
-        @ConfigName("Enable Non-vanilla Fish")
-        @Comment("Mod should not be removed if set to true")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Non-vanilla Fish",
+                multiplayerSynced = true
+        )
         public Boolean enableNonVanillaFish = false;
 
-        @ConfigName("Enable Oceanic Fish")
-        @Comment("Increases the max fish size from 70 to 110")
-        @MultiplayerSynced
+        @ConfigEntry(
+                name = "Enable Oceanic Fish",
+                description = "Increases the max fish size from 70 to 110",
+                multiplayerSynced = true
+        )
         public Boolean enableBiggerFish = false;
     }
 }
